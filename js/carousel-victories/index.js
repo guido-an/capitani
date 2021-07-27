f1db924321cb05576ded2e972348e623e9c8648b
gaurav tomar <gauravt@keymouseit.com>
2021-07-27 14:29:49 +0530

    Revert "All home page changes done"
    This reverts commit 23b464da32f6984f80a0a6bce9b06a600bfa22bb.

--- js/carousel-victories/index.js
+++ js/carousel-victories/index.js
@@ -1,3 +1,4 @@
+
 let firstElement = 0
 let secondElement = 1
 let thirdElement = 2
@@ -19,39 +20,32 @@ let thirdElement = 2
 //     `
 // }
 
-function displayVictories() {
-    return document.getElementById('carousel-victories').innerHTML +=
-        `<div id="victory-carousel-container">
+function displayVictories () {
+  return document.getElementById('carousel-victories').innerHTML +=
+  `<div id="victory-carousel-container">
     <div>
       <div class="carousel-img-container">
       <img class="carousel-img" src="${victories[firstElement].img}" alt="${victories[firstElement].name}">
-      <div class="caption">
-      <p class="victory-name">${victories[firstElement].name}</p>
-      <p class="victory-date">${victories[firstElement].date}</p>
-      </div>
      </div>
-    
+     <p class="victory-name">${victories[firstElement].name}</p>
+     <p class="victory-date">${victories[firstElement].date}</p>
     </div>
 
     <div>
       <div class="carousel-img-container" id="second-victory-container">
       <img class="carousel-img" src="${victories[secondElement].img}" alt="${victories[secondElement].name}">
-      <div class="caption">
-      <p class="victory-name">${victories[secondElement].name}</p>
-      <p class="victory-date">${victories[secondElement].date}</p>
-      </div>
      </div>
-    
+     <p class="victory-name">${victories[secondElement].name}</p>
+     <p class="victory-date">${victories[secondElement].date}</p>
+
     </div>
     <div>
       <div class="carousel-img-container" id="third-victory-container">
       <img class="carousel-img" src="${victories[thirdElement].img}" alt="${victories[thirdElement].name}" test="ciao">
-      <div class="caption">
+     </div>
      <p class="victory-name">${victories[thirdElement].name}</p>
      <p class="victory-date">${victories[thirdElement].date}</p>
-     </div>
-     </div>
-     
+
     </div>
 
   </div>
@@ -59,24 +53,24 @@ function displayVictories() {
     `
 }
 
-function hideVictories() {
-    return document.getElementById('carousel-victories').innerHTML = ''
+function hideVictories () {
+  return document.getElementById('carousel-victories').innerHTML = ''
 }
 
-window.addEventListener('load', function() {
-    displayVictories()
-    document.getElementById('next-arrow-victories').addEventListener('click', (e) => {
-        handleArrows(e, 'increase')
-    })
-    document.getElementById('prev-arrow-victories').addEventListener('click', (e) => {
-        handleArrows(e, 'decrease')
-    })
+window.addEventListener('load', function () {
+  displayVictories()
+  document.getElementById('next-arrow-victories').addEventListener('click', (e) => {
+    handleArrows(e, 'increase')
+  })
+  document.getElementById('prev-arrow-victories').addEventListener('click', (e) => {
+    handleArrows(e, 'decrease')
+  })
 
-    // var elements = document.getElementsByClassName('victory-img')
+  // var elements = document.getElementsByClassName('victory-img')
 
-    // for (var i = 0; i < elements.length; i++) {
-    //   elements[i].addEventListener('click', handleHoverImg, false)
-    // }
+  // for (var i = 0; i < elements.length; i++) {
+  //   elements[i].addEventListener('click', handleHoverImg, false)
+  // }
 })
 
 // var handleHoverImg = function () {
@@ -89,18 +83,18 @@ window.addEventListener('load', function() {
 //   // this.style.opacity = 1
 // }
 
-function handleArrows(e, operator) {
-    e.preventDefault()
-    if (operator === 'increase') {
-        firstElement === victories.length - 1 ? firstElement = 0 : firstElement++
-            secondElement === victories.length - 1 ? secondElement = 0 : secondElement++
-            thirdElement === victories.length - 1 ? thirdElement = 0 : thirdElement++
-    }
-    if (operator === 'decrease') {
-        firstElement === 0 ? firstElement = victories.length - 1 : firstElement--
-            secondElement === 0 ? secondElement = victories.length - 1 : secondElement--
-            thirdElement === 0 ? thirdElement = victories.length - 1 : thirdElement--
-    }
-    hideVictories()
-    displayVictories()
+function handleArrows (e, operator) {
+  e.preventDefault()
+  if (operator === 'increase') {
+    firstElement === victories.length - 1 ? firstElement = 0 : firstElement++
+    secondElement === victories.length - 1 ? secondElement = 0 : secondElement++
+    thirdElement === victories.length - 1 ? thirdElement = 0 : thirdElement++
+  }
+  if (operator === 'decrease') {
+    firstElement === 0 ? firstElement = victories.length - 1 : firstElement--
+    secondElement === 0 ? secondElement = victories.length - 1 : secondElement--
+    thirdElement === 0 ? thirdElement = victories.length - 1 : thirdElement--
+  }
+  hideVictories()
+  displayVictories()
 }
