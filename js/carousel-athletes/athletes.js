97be1d4c130db161caf5886e20227c2970ebf523
Guido <carucciguido@gmail.com>
2021-08-03 09:43:38 +0200

    last changes

--- js/carousel-athletes/athletes.js
+++ js/carousel-athletes/athletes.js
@@ -1,17 +1,17 @@
 
 const athletes = [
-  {
-    img: 'img/atleti/giampaolo-busbani.jpg',
-    name: 'giampaolo-busbani'
-  },
-  {
-    img: 'img/atleti/francesco-faggi.jpg',
-    name: 'francesco-faggi'
-  },
-  {
-    img: 'img/atleti/cini-fabio.jpg',
-    name: 'cini-fabio'
-  },
+  // {
+  //   img: 'img/atleti/giampaolo-busbani.jpg',
+  //   name: 'giampaolo-busbani'
+  // },
+  // {
+  //   img: 'img/atleti/francesco-faggi.jpg',
+  //   name: 'francesco-faggi'
+  // },
+  // {
+  //   img: 'img/atleti/cini-fabio.jpg',
+  //   name: 'cini-fabio'
+  // },
   {
     img: 'img/atleti/emanuel-de-martino.jpg',
     name: 'emanuel-de-martino'
@@ -32,11 +32,11 @@ const athletes = [
     img: 'img/gli-atleti/img-5.png',
     name: 'Evangelisti Emiliano De Martino Emanuel'
   },
-  {
-    img: 'img/gli-atleti/img-3.png',
-    name: 'Cini Fabio Faggi Francesco'
+  // {
+  //   img: 'img/gli-atleti/img-3.png',
+  //   name: 'Cini Fabio Faggi Francesco'
 
-  },
+  // },
   {
     img: 'img/gli-atleti/img-4.png',
     name: 'Mangiaterra Daniele Pisani Vincenzo'
@@ -44,6 +44,45 @@ const athletes = [
   {
     img: 'img/gli-atleti/img-2.png',
     name: 'Spaccia Valentino Virgili Fabio'
+  },
+  {
+    img: 'img/nuove-foto/1.jpg',
+    name: 'Capitani'
+  },
+  {
+    img: 'img/nuove-foto/2.jpg',
+    name: 'Capitani'
+  },
+  {
+    img: 'img/nuove-foto/3.jpg',
+    name: 'Capitani'
+  },
+  {
+    img: 'img/nuove-foto/4.jpg',
+    name: 'Capitani'
+  },
+  {
+    img: 'img/nuove-foto/5.jpg',
+    name: 'Capitani'
+  },
+  {
+    img: 'img/nuove-foto/6.jpg',
+    name: 'Capitani'
+  },
+  {
+    img: 'img/nuove-foto/7.jpg',
+    name: 'Capitani'
+  },
+  {
+    img: 'img/nuove-foto/8.jpg',
+    name: 'Capitani'
+  },
+  {
+    img: 'img/nuove-foto/9.jpg',
+    name: 'Capitani'
+  },
+  {
+    img: 'img/nuove-foto/10.jpg',
+    name: 'Capitani'
   }
-
 ]
